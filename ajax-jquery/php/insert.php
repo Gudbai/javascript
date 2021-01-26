1f2250a0107ef3f39a6fd939e8c45e2ae66e3bd5
Gudbai <aldienmaulana@gmail.com>
2021-01-26 16:36:32 +0700

    Version 4.00

---
+++ ajax-jquery/php/insert.php
@@ -0,0 +1,21 @@
+<?php 
+    $data = stripslashes(file_get_contents("php://input"));
+    $datapelanggan = json_decode($data, true);
+
+    $pelanggan = $datapelanggan['pelanggan'];
+    $alamat = $datapelanggan['alamat'];
+    $telp = $datapelanggan['telp'];
+
+    if (!empty($pelanggan) and !empty($alamat) and !empty($telp)) {
+        $sql = "INSERT INTO tblpelanggan VALUES('', '$pelanggan', '$alamat', '$telp')";
+        if ($result = mysqli_query($con, $sql)) {
+            echo "Data sudah disimpan!";
+        } else {
+            echo "Data gagal disimpan!";
+        }
+    } else {
+        echo "Data kosong";
+    }
+
+
+?>
